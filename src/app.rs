c7e5618b49e619e7ba2d671dc9da8849c6d0868f
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-05-18 02:24:55 -0400

    Working release with app file gui

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,15 @@
+#![allow(unused_imports)]
 use eframe::egui;
+use rfd::FileDialog;
+use std::{path::PathBuf, fs::File};
 
 #[derive(Default)]
-pub struct App {}
+pub struct App {
+	input_path: PathBuf,
+	output_path: String,
+	// output_format: String,
+	// log: Vec<String>
+}
 
 impl App {
 	pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -13,8 +21,43 @@ impl App {
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+		// egui::TopBottomPanel::new(egui::panel::TopBottomSide::Bottom, 2).show(ctx, |ui| {
+		// 	ui.heading("Status:");
+		// 	for msg in self.log.iter() {
+		// 		ui.label(msg);
+		// 	}
+		// });
 		egui::CentralPanel::default().show(ctx, |ui| {
 			ui.heading("Convi");
+			ui.label("Files");
+			ui.indent(2, |ui| {
+				ui.label("Input File:");
+				ui.text_edit_singleline(&mut self.input_path.clone().to_str().unwrap());
+				if ui.button("Set Input File").clicked() {
+					if let Some(p) = FileDialog::new().pick_file() {
+						self.input_path = p;
+					}
+				}
+
+				ui.label("Output File:");
+				ui.text_edit_singleline(&mut self.output_path);
+			});
+
+			ui.add_space(30_f32);
+			ui.heading("Output");
+			// ui.label("Format");
+			// ui.text_edit_singleline(&mut self.output_format);
+			
+			ui.add_space(5_f32);
+			if ui.button("Convert").clicked() {
+				println!("Spawning task with {} and {}",
+			&self.input_path.to_str().unwrap(), &self.output_path);
+				let _ = std::process::Command::new("ffmpeg")
+					.arg("-i")
+					.arg(self.input_path.to_str().unwrap())
+					.arg(&self.output_path)
+					.output();
+			}
 		});
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod app;
 use app::App;
+use eframe::emath::Vec2;
 
 fn main() {
 	let options = eframe::NativeOptions { 
 		always_on_top: false, 
 		decorated: true, 
 		drag_and_drop_support: true, 
-		// initial_window_pos: todo!(), 
-		// initial_window_size: todo!(), 
+		// initial_window_pos: Some(500), 
+		initial_window_size: Some(Vec2::new(500_f32, 500_f32)), 
 		resizable: true, 
 		// transparent: todo!(), 
 		vsync: true,
